fa619b03c6ca169ec9df7b4e189ad55a98dec8dd
BobbyK1996 <bobbykamal@hotmail.co.uk>
2024-01-29 11:50:58 +0000

    Basic first try using node-nlp

--- src/chatbot.js
+++ src/chatbot.js
@@ -1,1 +1,20 @@
-console.log('hi');
+import { NlpManager } from 'node-nlp';
+
+//New instance of NlpManager
+const manager = new NlpManager({ languages: ['en'] });
+
+//Add training data
+manager.addDocument('en', 'What is your name', 'bot.name');
+manager.addDocument('en', 'Where is your location', 'bot.location');
+manager.addDocument('en', 'What is your purpose', 'bot.purpose');
+
+//Train the model
+manager.train();
+
+//Process Input
+const userInput = async (input) => {
+  const response = await manager.process('en', input);
+  console.log(response);
+};
+
+userInput('What is your name');
