ab92e2f961d75bfdc54d3aaa8097fc4bd6c1269c
BobbyK1996 <bobbykamal@hotmail.co.uk>
2024-01-30 00:47:26 +0000

    Re-read the documentation and built the backend properly. Next step would be to use exoress to invoke the app with a request

--- src/chatbot.js
+++ src/chatbot.js
@@ -1,20 +1,43 @@
+// @ts-nocheck
 import { NlpManager } from 'node-nlp';
 
-//New instance of NlpManager
+//Instantiate NlpManager
 const manager = new NlpManager({ languages: ['en'] });
 
 //Add training data
-manager.addDocument('en', 'What is your name', 'bot.name');
-manager.addDocument('en', 'Where is your location', 'bot.location');
-manager.addDocument('en', 'What is your purpose', 'bot.purpose');
+manager.addDocument('en', 'hello', 'greeting');
+manager.addDocument('en', 'hi', 'greeting');
+manager.addDocument('en', 'hiya', 'greeting');
+manager.addDocument('en', 'yo', 'greeting');
+manager.addDocument('en', 'good morning', 'greeting');
+manager.addDocument('en', 'morning', 'greeting');
+manager.addDocument('en', 'good afternoon', 'greeting');
+manager.addDocument('en', 'afternoon', 'greeting');
+manager.addDocument('en', 'good evening', 'greeting');
+manager.addDocument('en', 'evening', 'greeting');
+manager.addDocument('en', 'good day', 'greeting');
+
+//Add answers
+manager.addAnswer('en', 'greeting', 'Hey!');
+manager.addAnswer('en', 'greeting', 'Hi');
+manager.addAnswer('en', 'greeting', 'Hiya');
+manager.addAnswer('en', 'greeting', 'Hey there');
+manager.addAnswer('en', 'greetings.bye', 'Cya');
+manager.addAnswer('en', 'greetings.bye', 'See you soon');
+manager.addAnswer('en', 'greetings.bye', 'Until next time');
+manager.addAnswer('en', 'greetings.bye', 'goodbye');
+manager.addAnswer('en', 'greetings.bye', 'later');
 
 //Train the model
-manager.train();
-
-//Process Input
-const userInput = async (input) => {
-  const response = await manager.process('en', input);
-  console.log(response);
+const trainAndSave = async () => {
+  try {
+    await manager.train();
+    manager.save();
+    const response = await manager.process('en', 'hey sir');
+    console.log(response);
+  } catch (er) {
+    console.error(er);
+  }
 };
 
-userInput('What is your name');
+trainAndSave();
